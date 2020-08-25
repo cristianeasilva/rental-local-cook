9985377399543b1eba9a104fd201a2d749f70058
leandro barroso <lrbarroso1984@gmail.com>
2020-08-25 14:15:54 -0300

    basic user features added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  # [...]
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :url_facebook, :url_instagram, :url_twitter, :nacionality, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :url_facebook, :url_instagram, :url_twitter])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :url_facebook, :url_instagram, :url_twitter])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
+  resources :cooks
   devise_for :users
   root to: 'cooks#index'
-  resources :cooks, only: [:index, :show, :create, :new] do
-    resources :orders do
-      resources :reviews
-    end
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'pages/home', to: 'pages#home'
 end
