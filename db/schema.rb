6a6212a1d5b909012e81e917179b450e0163b6cd
ewertonteixeira10 <ewerton.steixeira@gmail.com>
2020-08-25 14:55:34 -0300

    Updadting cooks mvc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_135752) do
+ActiveRecord::Schema.define(version: 2020_08_25_165522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_135752) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "service"
+    t.string "name"
     t.index ["user_id"], name: "index_cooks_on_user_id"
   end
 
