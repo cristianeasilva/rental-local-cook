6e6d5649d3baba9ee542ee97e8a6e1a1df79ce6e
leandro barroso <lrbarroso1984@gmail.com>
2020-08-25 23:17:49 -0300

    any orders features and more style

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :cooks, only: [:index, :show, :create, :new] do
-
+    resources :orders
   end
 
-  resources :orders
+  resources :orders, only: :show
 
   root to: 'cooks#index'
 
