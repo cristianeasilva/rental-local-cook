4ec5143e6f93a9491aa558f15273903feb1fff9f
ewertonteixeira10 <ewerton.steixeira@gmail.com>
2020-08-27 12:06:47 -0300

    Create search button

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -4,4 +4,15 @@ class Cook < ApplicationRecord
   has_one_attached :photo
 
   validates :location, :service, :name, :price, presence: true
+
+  include PgSearch::Model
+
+  pg_search_scope :search_infos,
+    against: [ :location, :price, :service, :name ],
+    associated_against: {
+      user: [:first_name, :last_name],
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
